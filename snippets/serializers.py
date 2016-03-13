414c287b4f7b3adb81220dc68b277cf7a96b7cff
Ed Solis <Ed@ALGLO.com>
2016-03-13 17:32:02 -0500

    Tutorial 4

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,9 +3,25 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields =    ('id', 'title', 'code', 'linenos', 'language', 'style', 
+                    'owner')
+
+
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True,
+        queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
+
 
 # class SnippetSerializer(serializers.Serializer):
 #     pk = serializers.IntegerField(read_only=True)
--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,9 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
 
 class SnippetList(generics.ListCreateAPIView):
     """
@@ -10,6 +13,10 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -17,3 +24,19 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    permission_classes = (  permissions.IsAuthenticatedOrReadOnly,
+                            IsOwnerOrReadOnly,)
+
+
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
